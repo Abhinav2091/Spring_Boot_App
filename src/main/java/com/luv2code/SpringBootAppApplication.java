b482eedd551997821ae0f90de2aa8674dd846874
abhinav2091 <abhinavsharma043@gmail.com>
2020-09-16 02:37:02 +0530

    just to check

--- src/main/java/com/luv2code/SpringBootAppApplication.java
+++ src/main/java/com/luv2code/SpringBootAppApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-		//(scanBasePackages = "package name") //need to done if you have other package outside your bse package for component scan
+//(scanBasePackages = "package name")
+// need to done if you have other package outside your bse package for component scan
 public class SpringBootAppApplication {
 
 	public static void main(String[] args) {
